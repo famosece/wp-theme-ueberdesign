55764709556e2629081d3fbacb15686135174d56
Beat Oberholzer <oberholzer@filmwords.com>
2013-04-17 09:07:01 +0200

    Remove title from content on standard pages. Fix #7

--- content.php
+++ content.php
@@ -1,11 +1,13 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
     <header class="entry-header">
+        <!--
     	<h1 class="entry-title">
     	    <a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'myThemeName' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark">
     	        <?php the_title(); ?>
     	    </a>
 	    </h1>
+	    -->
 
 		<div class="entry-meta"><!-- posted on -->
         </div><!-- .entry-meta -->
